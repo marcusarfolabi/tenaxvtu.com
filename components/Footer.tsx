665c09839bfc45bc0a2ff1a6a18e351494512142
MarcusAfolabi <biormarcus@gmail.com>
2026-02-15 14:25:28 +0300

    WIP

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,7 +24,7 @@ export default function Footer() {
       <div className="container mx-auto px-6">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12 mb-16">
           <div className="space-y-6"> 
-            <div className="dark">
+            <div>
               <Logo />
             </div>
 
